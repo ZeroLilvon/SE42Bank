185eed9967fb87fb8c948df731e6f961b37faa5a
Zeronium <riccardo.scoppetta@gmail.com>
2017-04-18 13:45:56 +0200

    TEST THINGS HAPPEND
    Unit = bankPU, staat in persistance.xml

---
+++ bank/src/test/java/opdracht1/opdracht1.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opdracht1;
+
+import bank.domain.Account;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import util.DatabaseCleaner;
+
+/**
+ *
+ * @author Riccardo
+ */
+public class opdracht1
+{
+    DatabaseCleaner DC;
+    
+    public opdracht1()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void Test1() 
+    {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("bankPU");
+        EntityManager em = emf.createEntityManager();
+        
+        Account account = new Account(111L);
+        em.getTransaction().begin();
+        em.persist(account);
+        //TODO: verklaar en pas eventueel aan
+        assertNull(account.getId());
+        em.getTransaction().commit();
+        System.out.println("AccountId: " + account.getId());
+        //TODO: verklaar en pas eventueel aan
+        assertTrue(account.getId() > 0L);
+
+    }
+}
