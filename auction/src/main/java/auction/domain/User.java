9af778aa6563c0a62e9d2faaf1b4636852f906d9
Zeronium <riccardo.scoppetta@gmail.com>
2017-05-02 15:18:40 +0200

    4 Done

--- auction/src/main/java/auction/domain/User.java
+++ auction/src/main/java/auction/domain/User.java
@@ -1,7 +1,19 @@
 package auction.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "User.findAll", query = "select u from User as u"),
+    @NamedQuery(name = "User.count", query = "select count(u) from User as u"),
+    @NamedQuery(name = "User.findByEmail", query = "select u from User as u where u.email = :userEmail")
+})
 public class User {
 
+    @Id  
     private String email;
 
     public User(String email) {
