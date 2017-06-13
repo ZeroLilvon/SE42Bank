0d09c8b42620a830660d1f7edc212803bbe2407f
Zeronium <riccardo.scoppetta@gmail.com>
2017-06-13 10:32:25 +0200

    HApp1Af

--- Encryption/src/encryption/Encryption.java
+++ Encryption/src/encryption/Encryption.java
@@ -5,6 +5,17 @@
  */
 package encryption;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Riccardo
@@ -17,7 +28,36 @@ public class Encryption
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        try
+        {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(1024);
+            KeyPair kp = kpg.genKeyPair();
+            Key publicKey = kp.getPublic();
+            Key privateKey = kp.getPrivate();
+            
+            FileOutputStream fileOut = new FileOutputStream("PublicH");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(publicKey);
+            fileOut.close();
+            out.close();
+            
+             fileOut = new FileOutputStream("PrivateH");
+             out = new ObjectOutputStream(fileOut);
+            out.writeObject(privateKey);
+            fileOut.close();
+            out.close();
+            
+        } catch (NoSuchAlgorithmException ex)
+        {
+            Logger.getLogger(Encryption.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (FileNotFoundException ex)
+        {
+            Logger.getLogger(Encryption.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex)
+        {
+            Logger.getLogger(Encryption.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
